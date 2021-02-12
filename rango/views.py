875494cda2228d7870c45aae47349ed5930b55fc
Danielius Sukys <2475466s@student.gla.ac.uk>
2021-02-12 17:05:46 +0000

    Chapter 8

--- rango/views.py
+++ rango/views.py
@@ -52,7 +52,7 @@ def add_category(request):
             form.save(commit=True)
             # Now that the category is saved, we could confirm this.
             # For now, just redirect the user back to the index view.
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             # The supplied form contained errors -
             # just print them to the terminal.
@@ -71,7 +71,7 @@ def add_page(request, category_name_slug):
         category = None
         
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
         
     
     form = PageForm()
